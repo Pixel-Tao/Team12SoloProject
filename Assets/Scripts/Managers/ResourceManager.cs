222688abae03abcd351c8f3e48db82f1cb06e979
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-11-13 11:39:10 +0900

    [Update] ResourceManager 기능 추가

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -1,5 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ResourceManager : Singleton<ResourceManager>
 {
-    
+    private Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
+    private Dictionary<string, Sprite> spriteDict = new Dictionary<string, Sprite>();
+
+    public T Load<T>(string path) where T : Object
+    {
+        System.Type t = typeof(T);
+        if (t == typeof(GameObject))
+        {
+            if (prefabDict.TryGetValue(path, out GameObject prefab) == false)
+            {
+                prefab = Resources.Load<GameObject>(path);
+                if (prefab != null)
+                    prefabDict.Add(path, prefab);
+            }
+            return prefab as T;
+        }
+
+        return null;
+    }
+    public Sprite Load(string filePath, bool isMultiple = false)
+    {
+        if (isMultiple)
+        {
+            // 파일 여러개에서 스프라이트 가져옴
+            string multipleSpriteName = filePath.Substring(filePath.LastIndexOf('/') + 1);
+            string filePathWithoutName = filePath.Substring(0, filePath.LastIndexOf('/'));
+            if (spriteDict.TryGetValue(filePath, out Sprite sprite) == false)
+            {
+                Sprite[] sprites = Resources.LoadAll<Sprite>(filePathWithoutName);
+                if (sprites.Length == 0)
+                {
+                    Debug.LogError($"Failed to load multiple sprite : {filePathWithoutName}");
+                    return null;
+                }
+
+                foreach (Sprite s in sprites)
+                {
+                    if (s.name == multipleSpriteName)
+                        sprite = s;
+                    
+                    spriteDict.Add($"{filePathWithoutName}/{s.name}", s);
+                }
+
+                if (sprite == null)
+                {
+                    Debug.LogError($"Failed to load sprite item : {filePath}");
+                    return null;
+                }
+            }
+
+            return sprite;
+        }
+        else
+        {
+            // 파일 하나에서 스프라이트 가져옴
+            if (spriteDict.TryGetValue(filePath, out Sprite sprite) == false)
+            {
+                sprite = Resources.Load<Sprite>(filePath);
+                if (sprite == null)
+                {
+                    Debug.LogError($"Failed to load single sprite : {filePath}");
+                    return null;
+                }
+                spriteDict.Add(filePath, sprite);
+            }
+
+            return sprite;
+        }
+    }
+    public GameObject Instantiate(string prefabPath, Transform parent = null)
+    {
+        GameObject prefab = Load<GameObject>(prefabPath);
+        if (prefab == null)
+        {
+            Debug.LogError($"Failed to load prefab : {prefabPath}");
+            return null;
+        }
+
+        return Instantiate(prefab, parent);
+    }
 }
--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -1,9 +1,17 @@
+using UnityEngine;
+
 public class GameScene : SceneBase
 {
     protected override void OnSceneLoad()
     {
         // 1. 씬 로드시 필요한 로직을 수행
         DBManager.Instance.Init();
+        ResourceManager.Instance.Init();
+
+        GameObject go = ResourceManager.Instance.Instantiate("Prefabs/Dummy");
+        go.transform.position = Vector3.zero;
+        SpriteRenderer sr = go.GetComponent<SpriteRenderer>();
+        sr.sprite = ResourceManager.Instance.Load("Textures/status/status_0", true);
     }
     protected override void OnSceneLoaded()
     {
